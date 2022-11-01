787a0f2f21f479367be1215cf06135b9c06d39d0
BryanSalgado2020 <bryansalgadoguz@gmail.com>
2022-10-31 20:25:37 -0500

    Movimiento de archivos y creacion de elementos block y function scope

---
+++ src/scope/block.js
@@ -0,0 +1,12 @@
+function fruit(){
+    if (true){
+        var fruit1= 'Apple';
+        let fruit2 = 'Piña';
+        const fruit3= 'Banana';
+    }
+    console.log(fruit1);
+    console.log(fruit2);
+    console.log(fruit3);
+}
+
+fruit();
